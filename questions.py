976d21bceca0d870778b7951d4e3f83c741811c9
Maximiliano Magro <magrokevinmax@gmail.com>
2025-03-24 12:49:43 -0300

    Tercera modificación

--- questions.py
+++ questions.py
@@ -23,15 +23,20 @@ correct_answers_index = [1, 2, 0, 3, 1]
 
 user_score = 0
 
+#Se crea una lista con las preguntas aleatorias
+questions_to_ask = random.choices(list(zip(questions,
+answers, correct_answers_index)), k=3)
+
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+for i in range(3):
+
+    #Guardo la posición de la respuesta correcta
+    correct_answer = questions_to_ask[i][2]
 
     # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+    print(questions_to_ask[i][0])
+    for a, answer in enumerate(questions_to_ask[i][1]):
+        print(f"{a + 1}. {answer}")
 
     # El usuario tiene 2 intentos para respondercorrectamente
     for intento in range(2):
@@ -40,7 +45,7 @@ for _ in range(3):
         if (user_answer.isnumeric() and 0<int(user_answer)<=4):
             user_answer = int(user_answer) - 1
             # Se verifica si la respuesta es correcta  
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == correct_answer:
                 print("¡Correcto!")
                 user_score += 1
                 break
@@ -53,10 +58,10 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(questions_to_ask[i][1][correct_answer])
         # Se imprime un blanco al final de la pregunta
         print()
-
+    print()
 #Muestro los puntajes
 if user_score <= 0:
     print("Puntaje: 0")
